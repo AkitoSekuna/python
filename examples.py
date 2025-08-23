57f3bd085788eae2c048dccad52b314b80c71594
Akito <akito.sekuna@gmail.com>
2025-08-23 16:18:14 +0100

    logic example added

---
+++ examples.py
@@ -0,0 +1,10 @@
+def example1():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}!")
+
+    number = int(input("Enter a number: "))
+    print(f"The square of {number} is {number ** 2}.")
+
+#decomment the function calls below to run the examples
+
+#example1() # This function prompts the user for their name and a number, then prints a greeting and the square of the number.
